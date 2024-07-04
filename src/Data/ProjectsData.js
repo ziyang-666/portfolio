36e817cf709a9ff3f5c7aa06f93192ca85f778d9
ziyang-666 <song.ziya@northeastern.edu>
2024-07-03 18:40:26 -0700

    update

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -1,13 +1,13 @@
 const DUMMY_PROJECTS = [
   {
-    projectTitle: "Uber eats",
-    description:
-      "Uber Eats is a food delivery service developed by Uber. It connects users with a wide range of restaurants, enabling them to order food through the Uber Eats app or website and have it delivered to their location. The platform leverages a complex tech stack and a variety of modern web development techniques to provide a seamless user experience.",
-  },
-  {
     projectTitle: "Hotel Search and Discovery",
     description:
       "The Hotel Search and Discovery project at Airbnb focuses on enhancing the user experience for finding and booking accommodations. The platform connects users with a wide range of properties, providing robust search and filtering capabilities to help them find the perfect stay. Leveraging a complex tech stack and modern web development techniques, the project aims to deliver a seamless and efficient user experience.",
+  },
+  {
+    projectTitle: "Storyscript",
+    description:
+      "Storyscript is a platform for creating and managing microservices workflows, allowing developers to build, deploy, and manage cloud-native applications seamlessly.",
   },
   {
     projectTitle: "Shopify Admin Dashboard",
